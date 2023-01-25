364f14a5485de3a146d73745e4e9e439a4eddbb2
bernoulliMukuna21 <mmbernoulli@gmail.com>
2023-01-25 12:51:02 +0000

    added jwt and google auth config variables

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -3,49 +3,95 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 interface IConfig {
-    port: string | number;
-    database: {
-        MONGODB_URI: string;
-        MONGODB_DB_MAIN: string;
-    };
-    secret: string;
+	port: string | number;
+	database: {
+		MONGODB_URI: string;
+		MONGODB_DB_MAIN: string;
+	};
+	secret: string;
+	auth: IAuth
+}
+
+interface IAuth{
+	google: IGoogleAuth
+	jwt: IJWT
+}
+
+interface IGoogleAuth{
+	clientID: string
+	clientSecret: string
+	callbackURL: string
+}
+
+interface IJWT{
+	secret: string
 }
 
 const NODE_ENV: string = process.env.NODE_ENV || 'development';
 
 const development: IConfig = {
-    port: process.env.PORT || 3000,
-    database: {
-        MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/',
-        MONGODB_DB_MAIN: process.env.MONGODB_DB_MAIN || 'users_db',
-    },
-    secret: process.env.SECRET || '@QEGTUI',
+	port: process.env.PORT || 3000,
+	database: {
+		MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/',
+		MONGODB_DB_MAIN: process.env.MONGODB_DB_MAIN || 'users_db',
+	},
+	secret: process.env.SECRET || '@QEGTUI',
+	auth: {
+		google: {
+			clientID: process.env.GOOGLE_CLIENT_ID || '',
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+			callbackURL: process.env.GOOGLE_CALLBACK_URL || ''
+		},
+		jwt: {
+			secret: process.env.JWT_SECRET || ''
+		}
+	}
 };
 
 const production: IConfig = {
-    port: process.env.PORT || 3000,
-    database: {
-        MONGODB_URI: process.env.MONGODB_URI || 'mongodb://production_uri/',
-        MONGODB_DB_MAIN: process.env.MONGODB_DB_MAIN || 'users_db',
-    },
-    secret: process.env.SECRET || '@QEGTUI',
+	port: process.env.PORT || 3000,
+	database: {
+		MONGODB_URI: process.env.MONGODB_URI || 'mongodb://production_uri/',
+		MONGODB_DB_MAIN: process.env.MONGODB_DB_MAIN || 'users_db',
+	},
+	secret: process.env.SECRET || '@QEGTUI',
+	auth: {
+		google: {
+			clientID: process.env.GOOGLE_CLIENT_ID || '',
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+			callbackURL: process.env.GOOGLE_CALLBACK_URL || ''
+		},
+		jwt: {
+			secret: process.env.JWT_SECRET || ''
+		}
+	}
 };
 
 const test: IConfig = {
-    port: process.env.PORT || 3000,
-    database: {
-        MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017',
-        MONGODB_DB_MAIN: 'test_users_db',
-    },
-    secret: process.env.SECRET || '@QEGTUI',
+	port: process.env.PORT || 3000,
+	database: {
+		MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017',
+		MONGODB_DB_MAIN: 'test_users_db',
+	},
+	secret: process.env.SECRET || '@QEGTUI',
+	auth: {
+		google: {
+			clientID: process.env.GOOGLE_CLIENT_ID || '',
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+			callbackURL: process.env.GOOGLE_CALLBACK_URL || ''
+		},
+		jwt: {
+			secret: process.env.JWT_SECRET || ''
+		}
+	}
 };
 
 const config: {
-    [name: string]: IConfig
+	[name: string]: IConfig
 } = {
-    test,
-    development,
-    production,
+	test,
+	development,
+	production,
 };
 
 export default config[NODE_ENV];
