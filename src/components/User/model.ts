21b0172a9191e23d2488d46a416cced65ee4e5f5
bernoulliMukuna21 <mmbernoulli@gmail.com>
2023-01-25 12:49:31 +0000

    added extra fields to model

--- src/components/User/model.ts
+++ src/components/User/model.ts
@@ -1,125 +1,136 @@
-import * as bcrypt from 'bcrypt';
-import * as crypto from 'crypto';
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 import { Document, Schema } from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+import mongooseUniqueValidator from 'mongoose-unique-validator';
 import * as connections from '../../config/connection/connection';
 
-export type AuthToken = {
-    accessToken: string,
-    kind: string,
-};
-
-/**
- * @export
- * @interface IUserModel
- * @extends {Document}
- */
-export interface IUserModel extends Document {
-    email: string;
-    password: string;
-    passwordResetToken: string;
-    passwordResetExpires: Date;
-
-    facebook: string;
-    tokens: AuthToken[];
-
-    profile: {
-        name: string,
-        gender: string,
-        location: string,
-        website: string,
-        picture: string,
-    };
-    comparePassword: (password: string) => Promise < boolean > ;
-    gravatar: (size: number) => string;
+enum ROLES {
+  ADMIN = 'admin',
+  KINGLANCER = 'kinglancer',
+  CLIENT = 'client',
 }
 
-/**
- * @swagger
- * components:
- *  schemas:
- *    UserSchema:
- *      required:
- *        - email
- *        - name
- *      properties:
- *        id:
- *          type: string
- *        name:
- *          type: string
- *        email:
- *          type: string
- *        password:
- *          type: string
- *        passwordResetToken:
- *          type: string
- *        passwordResetExpires:
- *          type: string
- *          format: date
- *        tokens:
- *          type: array
- *    Users:
- *      type: array
- *      items:
- *        $ref: '#/components/schemas/UserSchema'
- */
+export interface IUserModel extends Document {
+	username: string
+	fullname: string
+	email: string
+	role: string
+	description: string
+	services: Services[]
+	skills: Skills[]
+	password: string
+	passwordResetToken: string
+	passwordResetExpires: Date
+	google: string
+	token: AuthToken
+	profile: {
+		name: string,
+		gender: string,
+		picture: string,
+	}
+	comparePassword: (password: string) => Promise < boolean > 
+	gravatar: (size: number) => string
+}
+
+export type AuthToken = {
+	accessToken?: string,
+	kind: string,
+	refreshToken?: string
+}
+
+export type Services = {
+	service: string
+	price: string
+	currency: string
+	servicePackage: string
+}
+
+export type Skills = {
+	title: string
+}
+
 const UserSchema: Schema = new Schema({
-    email: {
-        type: String,
-        unique: true,
-        trim: true,
-    },
-    password: String,
-    passwordResetToken: String,
-    passwordResetExpires: Date,
-    tokens: Array,
+	fullName: {
+		type: String,
+		trim: true
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true
+	},
+	email: {
+		type: String,
+		unique: true,
+		trim: true,
+	},
+	role: {
+		type: String,
+		enum: Object.values(ROLES),
+		default: ROLES.CLIENT
+	},
+	password: {
+		type: String,
+		minlength: 6
+	},
+	auth: {
+		google: {
+			id: String,
+			token: String
+		}
+	},
+	services: Array,
+	description: String,
+	skills: Array,
+	profilePicture: String,
+	averageRating: Number,
+	passwordResetToken: String,
+	passwordResetExpires: Date,
 }, {
-    collection: 'usermodel',
-    versionKey: false,
-}).pre('save', async function (next): Promise < void > {
-    const user: IUserModel = this; // tslint:disable-line
-
-    if (!user.isModified('password')) {
-        return next();
-    }
-
-    try {
-        const salt: string = await bcrypt.genSalt(10);
-
-        const hash: string = await bcrypt.hash(user.password, salt);
-
-        user.password = hash;
-        next();
-    } catch (error: any) {
-        return next(error);
-    }
+	collection: 'usermodel',
+	versionKey: true,
+	timestamps: true
 });
 
-/**
- * Method for comparing passwords
- */
+UserSchema.pre('save', async function (next): Promise < void > {
+	const user: IUserModel = this; // tslint:disable-line
+	if (!user.isModified('password')) {
+		return next();
+	}
+
+	try {
+		const salt: string = await bcrypt.genSalt(10);
+		const hash: string = await bcrypt.hash(user.password, salt);
+		user.password = hash;
+		next();
+	} catch (error: any) {
+		return next(error);
+	}
+});
+
 UserSchema.methods.comparePassword = async function (candidatePassword: string): Promise < boolean > {
-    try {
-        const match: boolean = await bcrypt.compare(candidatePassword, this.password);
-
-        return match;
-    } catch (error: any) {
-        return error;
-    }
+	try {
+		const match: boolean = await bcrypt.compare(candidatePassword, this.password);
+		return match;
+	} catch (error: any) {
+		return error;
+	}
 };
 
-/**
- * Helper method for getting user's gravatar.
- */
 UserSchema.methods.gravatar = function (size: number): string {
-    if (!size) {
-        size = 200;
-    }
-    if (!this.email) {
-        return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-    }
-    const md5: string = crypto.createHash('md5').update(this.email).digest('hex');
-
-    return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+	if (!size) {
+		size = 200;
+	}
+	if (!this.email) {
+		return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+	}
+	const md5: string = crypto.createHash('md5').update(this.email).digest('hex');
+	return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
 };
+
+UserSchema.plugin(mongoosePaginate);
+UserSchema.plugin(mongooseUniqueValidator);
 
 export default connections.db.model< IUserModel >('UserModel', UserSchema);
