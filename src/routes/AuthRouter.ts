8108e13360072b89a9fc9ce28a46c311becbcd71
bernoulliMukuna21 <mmbernoulli@gmail.com>
2023-01-25 13:35:24 +0000

    add more routes

--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -5,5 +5,10 @@ const router: Router = Router();
 
 router.post('/signup', AuthComponent.signup);
 router.post('/login', AuthComponent.login);
+router.post('/logout', AuthComponent.login);
+router.post('/forgot-password', AuthComponent.login);
+router.post('/reset-password', AuthComponent.login);
+router.post('/refresh-token', AuthComponent.login);
+router.post('/google', AuthComponent.login);
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,12 +8,14 @@ import AuthRouter from './AuthRouter';
 import UserRouter from './UserRouter';
 
 const swaggerDef = require('../../swaggerDef');
+const rootApi = '/api/v1';
+
 export function init(app: express.Application): void {
 	const router: express.Router = express.Router();
 
-	app.use('/v1/users', jwtConfig.isAuthenticated, UserRouter);
-	app.use('/auth', AuthRouter);
-	app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc({
+	app.use(`${rootApi}/users`, jwtConfig.isAuthenticated, UserRouter);
+	app.use(`${rootApi}/auth`, AuthRouter);
+	app.use(`${rootApi}/docs`, swaggerUi.serve, swaggerUi.setup(swaggerJSDoc({
 		swaggerDefinition: swaggerDef,
 		apis: [path.join(__dirname, '../../documentation/**/*.yml')],
 	})));
