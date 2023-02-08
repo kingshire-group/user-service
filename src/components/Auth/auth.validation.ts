820567fe40f975c078157c20b04a99b679819652
bernoulliMukuna21 <mmbernoulli@gmail.com>
2023-02-08 15:08:35 +0000

    trim all strings before validation

--- src/components/Auth/auth.validation.ts
+++ src/components/Auth/auth.validation.ts
@@ -1,20 +1,22 @@
 import Joi from "joi"
 
 const passwordMatcher =
-      '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'
+  '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'
 const usernameMatcher =
-      '^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._£$€]+(?<![_.])$'
+	'^(?=[a-zA-Z0-9._£$€]{3,20}$)(?!.*[_.]{2})[^_.].*[^_.]$'
 
 export const emailSchemaField = { 
-	email: Joi.string()
+	email: Joi.string().trim()
 	.email({minDomainSegments: 2}).required() } 
   
 export const passwordSchemaField = { 
-	password: Joi.string()
+	password: Joi.string().trim()
 		.pattern(new RegExp(passwordMatcher)).required() } 
 
 export const usernameSchemaField = { 
-	username: Joi.string()
+	username: Joi.string().trim()
 		.pattern(new RegExp(usernameMatcher)).required() }
 
-export const fullnameSchemaField = { fullname: Joi.string().required() } 
+export const fullnameSchemaField = {
+	fullname: Joi.string().trim().min(5).required()
+} 
